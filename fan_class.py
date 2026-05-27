c1fcb1e62319d86cf6b34ce770374d48383a94ae
renanthonymasadao-coder <renanthony.masadao@gmail.com>
2026-05-27 20:28:36 +0800

    adding constant values and private attributes

--- fan_class.py
+++ fan_class.py
@@ -1,3 +1,15 @@
 #import libraries
 class Fan:
+
+    # Constants values
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 3
+
+    def __init__(self, speed=SLOW, radius=5, color="blue", on=False):
+        # Private Attributes (Encapsulation)
+        self.__speed = speed
+        self.__radius = radius
+        self.__color = color
+        self.__on = on
     
