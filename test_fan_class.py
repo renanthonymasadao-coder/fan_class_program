46643d5e6e878be27a1a24492d00ed16cb1ec553
renanthonymasadao-coder <renanthony.masadao@gmail.com>
2026-05-27 21:23:29 +0800

    adding first object of fan to the code

--- test_fan_class.py
+++ test_fan_class.py
@@ -1,3 +1,14 @@
 # test_fan.py, importing libraries to test fan class program
 
 from fan import Fan
+
+print("\n========== FAN CLASS TEST PROGRAM ==========\n")
+
+# FIRST FAN OBJECT
+fan1 = Fan()
+
+fan1.set_speed(Fan.FAST)
+fan1.set_radius(10)
+fan1.set_color("yellow")
+fan1.set_on(True)
+
