2eed7aaf9971ad0402161cde82e54b7ef3367f8d
renanthonymasadao-coder <renanthony.masadao@gmail.com>
2026-05-27 21:28:30 +0800

    adding second object code for fan class program

--- test_fan_class.py
+++ test_fan_class.py
@@ -12,3 +12,10 @@ fan1.set_radius(10)
 fan1.set_color("yellow")
 fan1.set_on(True)
 
+# SECOND FAN OBJECT
+fan2 = Fan()
+
+fan2.set_speed(Fan.MEDIUM)
+fan2.set_radius(5)
+fan2.set_color("blue")
+fan2.set_on(False)
